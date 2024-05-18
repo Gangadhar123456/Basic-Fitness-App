878abe241800efd2d9a07be4b653e29860970062
gangadhar <gangadharsesetti13@gmail.comm>
2024-05-18 09:34:58 +0530

    commit message

---
+++ src/Workouts.js
@@ -0,0 +1,13 @@
+// Workouts.js
+import React from 'react';
+
+function Workouts() {
+  return (
+    <div>
+      <h2>Log Your Workouts</h2>
+      <p>Workout logging form will go here...</p>
+    </div>
+  );
+}
+
+export default Workouts;
